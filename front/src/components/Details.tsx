b9ee4840464aad3f076ad3ca34b6520196fd4ac0
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-02-14 10:11:15 +0200

    add pagination

--- front/src/components/Details.tsx
+++ front/src/components/Details.tsx
@@ -1,17 +1,39 @@
 import { Paste } from '../types/details.types';
 import ControlledAccordions from './ControlledAccordions';
+import '../styles/details.css';
+import PaginationComponent from './PaginationComponent';
+import { useState } from 'react';
 
 const Details = ({ pastes }: { pastes: Paste[] }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pastesPerPage] = useState(25);
+
+  const indexOfLastPaste = currentPage * pastesPerPage; // 1 * 4 = 4
+  const indexOfFirstPaste = indexOfLastPaste - pastesPerPage; // 4 - 4 = 0
+  const currentPastes = pastes.slice(indexOfFirstPaste, indexOfLastPaste); // 0 1 2 3
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber); // change the current page
+    // scrollUpBtn.current.click(); // click on the scroll up
+  };
+
   return (
     <div>
       <h2>Details</h2>
-      {pastes
+      {/*if the data is not loaded yet, show loader*/}
+      {!pastes.length && <span className="loader">Load&nbsp;ng</span>}
+      {currentPastes
         .sort((a, b) => +new Date(b.date) - +new Date(a.date)) // sort by date
         .map((paste, i) => (
           <div className="paste-div" key={i}>
             <ControlledAccordions paste={paste} />
           </div>
         ))}
+      <PaginationComponent
+        productsPerPage={pastesPerPage}
+        totalProducts={pastes.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
---
+++ front/src/components/PaginationComponent.tsx
@@ -0,0 +1,19 @@
+import '../styles/paginationComponent.css';
+import Pagination from '@mui/material/Pagination';
+// import { PaginationComponentProps } from '../../types/PaginationComponent.types';
+
+const PaginationComponent = ({ productsPerPage, totalProducts, paginate }: any) => {
+  const pageNumbers = Math.ceil(totalProducts / productsPerPage); // total number of page
+  return (
+    <div className="pagination-div">
+      <Pagination
+        count={pageNumbers}
+        onChange={(event: React.ChangeEvent<unknown>, page: number) => {
+          paginate(page);
+        }}
+      />
+    </div>
+  );
+};
+
+export default PaginationComponent;
