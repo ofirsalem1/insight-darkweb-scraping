ade0d4ec61b28f2872b3a97d8d35d5ff16473b74
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-02-17 14:54:38 +0200

    add chart of author

--- front/src/components/Analysis.tsx
+++ front/src/components/Analysis.tsx
@@ -25,6 +25,15 @@ const Analysis = ({ pastes }: { pastes: Paste[] }) => {
     if (paste.content.toLowerCase().includes('spyware')) wordsContentsObj.spyware++;
   }
 
+  const authorsObj: { [key: string]: number } = {};
+  for (const paste of pastes) {
+    if (paste.author in authorsObj) {
+      authorsObj[paste.author]++;
+    } else {
+      authorsObj[paste.author] = 1;
+    }
+  }
+
   const dataTitles = {
     labels: ['Porn', 'Bitcoin', 'Drug', 'Hacker', 'Orgy', 'Spyware'],
     datasets: [
@@ -92,18 +101,57 @@ const Analysis = ({ pastes }: { pastes: Paste[] }) => {
     ],
   };
 
+  const dataAuthors = {
+    labels: [...Object.keys(authorsObj)],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [...Object.values(authorsObj)],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div>
-      <div className="pie-chart">
-        <div>
-          <h3>Title - {Object.values(wordsTitleObj).reduce((a, b) => a + b)}</h3>
-          <Pie data={dataTitles} />
+      {pastes.length === 0 && <span className="loader"></span>}
+      {pastes.length > 0 && (
+        <div className="pie-chart">
+          <div>
+            <h3>Title </h3>
+            <h4>Total number - {Object.values(wordsTitleObj).reduce((a, b) => a + b)}</h4>
+            <Pie data={dataTitles} />
+          </div>
+          <div>
+            <h3>Content </h3>
+            <h4>Total number - {Object.values(wordsContentsObj).reduce((a, b) => a + b)}</h4>
+
+            <Pie data={dataContents} />
+          </div>
+          <div>
+            <h3>Paste Per Author </h3>
+            <h4>Total number - {Object.values(authorsObj).reduce((a, b) => a + b)}</h4>
+
+            <Pie data={dataAuthors} />
+          </div>
         </div>
-        <div>
-          <h3>Content - {Object.values(wordsContentsObj).reduce((a, b) => a + b)}</h3>
-          <Pie data={dataContents} />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
